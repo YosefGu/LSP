3a587ee58a38a2f598a5c9ba19b216d14e0d545c
YosefGu <yos770yos@gmail.com>
2025-06-19 12:46:03 +0300

    Exercise 2 complete

---
+++ Exercise2/Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP.Exercise2
+{
+    internal class Main
+    {
+        public static void RunExercise2()
+        {
+            Rectangle rectangle = new Rectangle();
+            Sqare sqare = new Sqare();
+
+            //TestArea(rectangle);
+            //TestArea(sqare);
+            Console.WriteLine(rectangle.Area());
+            Console.WriteLine(sqare.Area());
+
+        }
+        //public static void TestArea(Rectangle shape) 
+        //{
+        //    shape.Height = 10;
+        //    shape.Width = 5;
+
+        //    Console.WriteLine(shape.Area());
+
+        //}
+    }
+}
---
+++ Exercise2/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP.Exercise2
+{
+    internal class Rectangle
+    {
+        public virtual int Height { get; set; }
+        public virtual int Width { get; set; }
+
+        public int Area() 
+        {
+            return Height * Width;
+        }
+    }
+}
